9cea0d455d95f074cc331eeadaa0e15adc06ad87
zaofengyue <yzfywgck@outlook.com>
2026-07-09 07:03:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   site: {
     title: process.env.SITE_TITLE || '风月的导航站-个人专属导航页',
-    logo: process.env.SITE_LOGO || '',
+    logo: process.env.SITE_LOGO || 'https://image.yzfy.dpdns.org/2026/07/d420c456668e2bb6180919ba51c45fdf.png',
     background: process.env.SITE_BACKGROUND || 'https://image.yzfy.dpdns.org/2026/05/3701899e7ceb71d01acbdfa267d90e31.png'
   }
 }; 
